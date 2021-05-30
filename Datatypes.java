25644d8eb70cf70a45c161305b2ddc40fb3953b7
akankshapolasa <84825249+akankshapolasa@users.noreply.github.com>
2021-05-30 19:15:14 +0530

    Add files via upload

---
+++ Datatypes.java
@@ -0,0 +1,18 @@
+class Datatypes{
+public static void main(String args[]){
+byte b=0;
+short s=0;
+int i=0;
+long l=0;
+float f=0.0F;
+double d=0.0;
+String str=null;
+System.out.println("Byte value::"+b);
+System.out.println("Short value::"+s);
+System.out.println("Integer value::"+i);
+System.out.println("Long value::"+l);
+System.out.println("Float value::"+f);
+System.out.println("Double value::"+d);
+System.out.println("String value::"+str);
+}
+}
