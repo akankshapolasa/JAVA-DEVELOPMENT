60af25acdaf0af0b094cd08628d80d9ac54d47ee
akankshapolasa <84825249+akankshapolasa@users.noreply.github.com>
2021-05-30 19:32:48 +0530

    Add files via upload

---
+++ Datatypes2.java
@@ -0,0 +1,18 @@
+class Datatypes2{
+public static void main(String args[]){
+byte day=7;
+short days=365;
+int age=21;
+long mobileNumber=905920783;
+float price=700.0F;
+double amount=100000.2525;
+String name="Chinny";
+System.out.println("Day value::"+day);
+System.out.println("Days value::"+days);
+System.out.println("Age value::"+age);
+System.out.println("MobileNumber::"+mobileNumber);
+System.out.println("Price::"+price);
+System.out.println("Amount::"+amount);
+System.out.println("Name::"+name);
+}
+}
